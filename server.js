16253a307198d3bad73b48e3e792e14d81200747
Midhun Paul <midhunpaul396@gmail.com>
2024-10-19 02:31:23 -0400

    json change

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://mid-invent-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://mid-invent-app.onrender.com"],
     credentials: true,
   })
 );
