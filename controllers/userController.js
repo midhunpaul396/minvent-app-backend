8e40949334423eec93806a66c03da05f5a7f2e8c
Midhun Paul <midhunpaul396@gmail.com>
2024-10-19 21:25:43 -0400

    tokenfix

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
   if (password.length < 6) {
     res.status(400);
-    throw new Error("Password must be up to 6 characters");
+    throw new Error("Password must be at least 6 characters");
   }
 
   // Check if user email already exists
@@ -40,16 +40,16 @@ const registerUser = asyncHandler(async (req, res) => {
     password,
   });
 
-  //   Generate Token
+  // Generate Token
   const token = generateToken(user._id);
 
   // Send HTTP-only cookie
   res.cookie("token", token, {
     path: "/",
     httpOnly: true,
-    expires: new Date(Date.now() + 1000 * 86400), // 1 day
-    sameSite: "none",
-    secure: true,
+    expires: new Date(Date.now() + 1000 * 86400), // 1 day expiry
+    sameSite: "None", // Allows cross-origin requests
+    secure: true, // Ensures the cookie is only sent over HTTPS
   });
 
   if (user) {
@@ -84,25 +84,26 @@ const loginUser = asyncHandler(async (req, res) => {
 
   if (!user) {
     res.status(400);
-    throw new Error("User not found, please signup");
+    throw new Error("User not found, please sign up");
   }
 
-  // User exists, check if password is correct
+  // Check if password is correct
   const passwordIsCorrect = await bcrypt.compare(password, user.password);
 
-  //   Generate Token
+  // Generate Token
   const token = generateToken(user._id);
-  
-  if(passwordIsCorrect){
-   // Send HTTP-only cookie
-  res.cookie("token", token, {
-    path: "/",
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000 * 86400), // 1 day
-    sameSite: "none",
-    secure: true,
-  });
-}
+
+  if (passwordIsCorrect) {
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400), // 1 day expiry
+      sameSite: "None", // Necessary for cross-origin requests
+      secure: true, // Ensures cookie is sent only over HTTPS
+    });
+  }
+
   if (user && passwordIsCorrect) {
     const { _id, name, email, photo, phone, bio } = user;
     res.status(200).json({
@@ -125,8 +126,8 @@ const logout = asyncHandler(async (req, res) => {
   res.cookie("token", "", {
     path: "/",
     httpOnly: true,
-    expires: new Date(0),
-    sameSite: "none",
+    expires: new Date(0), // Expire the cookie immediately
+    sameSite: "None",
     secure: true,
   });
   return res.status(200).json({ message: "Successfully Logged Out" });
@@ -193,21 +194,23 @@ const updateUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Change Password
 const changePassword = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
   const { oldPassword, password } = req.body;
 
   if (!user) {
     res.status(400);
-    throw new Error("User not found, please signup");
+    throw new Error("User not found, please sign up");
   }
-  //Validate
+
+  // Validate input
   if (!oldPassword || !password) {
     res.status(400);
     throw new Error("Please add old and new password");
   }
 
-  // check if old password matches password in DB
+  // Check if old password matches
   const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
 
   // Save new password
@@ -221,97 +224,6 @@ const changePassword = asyncHandler(async (req, res) => {
   }
 });
 
-const forgotPassword = asyncHandler(async (req, res) => {
-  const { email } = req.body;
-  const user = await User.findOne({ email });
-
-  if (!user) {
-    res.status(404);
-    throw new Error("User does not exist");
-  }
-
-  // Delete token if it exists in DB
-  let token = await Token.findOne({ userId: user._id });
-  if (token) {
-    await token.deleteOne();
-  }
-
-  // Create Reste Token
-  let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
-  console.log(resetToken);
-
-  // Hash token before saving to DB
-  const hashedToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-
-  // Save Token to DB
-  await new Token({
-    userId: user._id,
-    token: hashedToken,
-    createdAt: Date.now(),
-    expiresAt: Date.now() + 30 * (60 * 1000), // Thirty minutes
-  }).save();
-
-  // Construct Reset Url
-  const resetUrl = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
-
-  // Reset Email
-  const message = `
-      <h2>Hello ${user.name}</h2>
-      <p>Please use the url below to reset your password</p>  
-      <p>This reset link is valid for only 30minutes.</p>
-
-      <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
-
-      <p>Regards...</p>
-      <p>Midhun Inventory</p>
-    `;
-  const subject = "Password Reset Request";
-  const send_to = user.email;
-  const sent_from = process.env.EMAIL_USER;
-
-  try {
-    await sendEmail(subject, message, send_to, sent_from);
-    res.status(200).json({ success: true, message: "Reset Email Sent" });
-  } catch (error) {
-    res.status(500);
-    throw new Error("Email not sent, please try again");
-  }
-});
-
-// Reset Password
-const resetPassword = asyncHandler(async (req, res) => {
-  const { password } = req.body;
-  const { resetToken } = req.params;
-
-  // Hash token, then compare to Token in DB
-  const hashedToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-
-  // fIND tOKEN in DB
-  const userToken = await Token.findOne({
-    token: hashedToken,
-    expiresAt: { $gt: Date.now() },
-  });
-
-  if (!userToken) {
-    res.status(404);
-    throw new Error("Invalid or Expired Token");
-  }
-
-  // Find user
-  const user = await User.findOne({ _id: userToken.userId });
-  user.password = password;
-  await user.save();
-  res.status(200).json({
-    message: "Password Reset Successful, Please Login",
-  });
-});
-
 module.exports = {
   registerUser,
   loginUser,
@@ -320,6 +232,4 @@ module.exports = {
   loginStatus,
   updateUser,
   changePassword,
-  forgotPassword,
-  resetPassword,
 };
